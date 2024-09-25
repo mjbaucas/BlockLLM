9b5502c5fb36a687e0d6d79e88516383e893f7e2
mjbaucas <mjbaucas@gmail.com>
2024-09-25 16:11:27 -0400

    Changed local llm code

--- localllm.py
+++ localllm.py
@@ -1,22 +1,8 @@
 import sys
-from langchain_community.llms import LlamaCpp
+from langchain_community.llms import Ollama
 
-# enable verbose to debug the LLM's operation
-verbose = False
-
-llm = LlamaCpp(
-    model_path="synthia-7b-v2.0-16k.Q4_K_M.gguf",
-    # max tokens the model can account for when processing a response
-    # make it large enough for the question and answer
-    n_ctx=4096,
-    # number of layers to offload to the GPU 
-    # GPU is not strictly required but it does help
-    n_gpu_layers=32,
-    # number of tokens in the prompt that are fed into the model at a time
-    n_batch=1024,
-    # use half precision for key/value cache; set to True per langchain doc
-    f16_kv=True,
-    verbose=verbose,
+llm = Ollama(
+    model="tinyllama",
 )
 
 while True:
@@ -24,12 +10,6 @@ while True:
     if question == "stop":
         sys.exit(1)
     output = llm.invoke(
-        question,
-        max_tokens=4096,
-        temperature=0.2,
-        # nucleus sampling (mass probability index)
-        # controls the cumulative probability of the generated tokens
-        # the higher top_p the more diversity in the output
-        top_p=0.1
+        question
     )
     print(f"\n{output}") 
