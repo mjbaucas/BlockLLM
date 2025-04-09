52c6baec2e40db8036bef263668084d4f212d040
mjbaucas <mjbaucas@gmail.com>
2025-04-09 17:28:56 -0400

    added client code

---
+++ client.py
@@ -0,0 +1,26 @@
+import socket
+import time 
+
+reset = 1
+start = 0
+while True:
+	if reset == 1:
+		start = time.time()
+		reset = 0
+	try:
+		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		s.connect(("10.12.206.65", 5000))
+		s.sendall(bytes('Can you give me a recipe for banana bread?', "utf-8"))
+		message = s.recv(1024).decode("utf-8")
+		if message  != "":
+			end = time.time()
+			elapsed = end-start
+			if elapsed > 1.0:
+				print(elapsed)
+			reset = 1
+		else:
+			reset = 0
+		s.close()
+	except Exception as msg:
+		print(msg)
+		reset = 0
