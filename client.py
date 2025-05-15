81cbef9d9bb1bc724c30f2edc28cb1602d59b7cd
mjbaucas <mjbaucas@gmail.com>
2025-05-14 21:20:58 -0400

    testing changes

--- client.py
+++ client.py
@@ -13,7 +13,7 @@ while True:
 	try:
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		s.connect(("10.12.202.68", 5000))
-		s.sendall(bytes('What is cruise control, and how does it help during long trips?', "utf-8"))
+		s.sendall(bytes('Define BMI and explain how to calculate it.', "utf-8"))
 		message = s.recv(1024).decode("utf-8")
 		if message  != "":
 			end = time.time()
